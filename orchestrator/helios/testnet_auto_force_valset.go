ede3a92cae019950872255dc27e229dc43700576
Sotatek-CuongTran <cuong.tran@sotatek.com>
2025-04-08 15:14:57 +0700

    refactor: refactor code for testnet

---
+++ orchestrator/helios/testnet_auto_force_valset.go
@@ -0,0 +1,72 @@
+package helios
+
+import (
+	"context"
+	"github.com/Helios-Chain-Labs/hyperion/orchestrator/ethereum"
+	
+	sdkmath "cosmossdk.io/math"
+	hyperiontypes "github.com/Helios-Chain-Labs/sdk-go/chain/hyperion/types"
+	log "github.com/xlab/suplog"
+)
+
+func TestnetForceUpdateValset(
+	ctx context.Context,
+	hyperionID int,
+	heliosNetwork Network,
+	ethNetwork ethereum.Network,
+) error {
+	// check if the helios hyperion is not synchronized
+	nonce, err := heliosNetwork.QueryGetLastObservedEventNonce(ctx, uint64(hyperionID))
+	if err != nil {
+		return err
+	}
+	lastEventNonce, err := ethNetwork.GetLastEventNonce(ctx)
+	if err != nil {
+		return err
+	}
+	if nonce == 0 && lastEventNonce.Uint64() > 1 { // not firstime
+
+		height, err := ethNetwork.GetLastValsetUpdatedEventHeight(ctx)
+		if err != nil {
+			return err
+		}
+
+		lastEventBlockHeight, err := ethNetwork.GetLastEventHeight(ctx)
+		if err != nil {
+			return err
+		}
+
+		events, err := ethNetwork.GetValsetUpdatedEventsAtSpecificBlock(height.Uint64())
+		if err != nil {
+			return err
+		}
+
+		if len(events) == 0 {
+			log.Fatalln("helios hyperion is not synchronized, please wait for it to be synchronized")
+		}
+
+		event := events[0]
+
+		valset := &hyperiontypes.Valset{
+			Nonce:        event.NewValsetNonce.Uint64(),
+			Members:      make([]*hyperiontypes.BridgeValidator, 0, len(event.Powers)),
+			RewardAmount: sdkmath.NewIntFromBigInt(event.RewardAmount),
+			RewardToken:  event.RewardToken.Hex(),
+		}
+
+		for idx, p := range event.Powers {
+			valset.Members = append(valset.Members, &hyperiontypes.BridgeValidator{
+				Power:           p.Uint64(),
+				EthereumAddress: event.Validators[idx].Hex(),
+			})
+		}
+
+		err = heliosNetwork.SendForceSetValsetAndLastObservedEventNonce(ctx, uint64(hyperionID), lastEventNonce.Uint64(), lastEventBlockHeight.Uint64(), valset)
+		if err != nil {
+			return err
+		}
+
+		log.Infoln("helios hyperion is now forcefully synchronized with ethereum hyperion")
+	}
+	return nil
+}
---
+++ orchestrator/helios/testnet_auto_register.go
@@ -0,0 +1,32 @@
+package helios
+
+import (
+	"context"
+
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	ethcmn "github.com/ethereum/go-ethereum/common"
+	log "github.com/xlab/suplog"
+)
+
+func TestnetAutoRegisterValidator(
+	ctx context.Context,
+	hyperionID int,
+	heliosNetwork Network,
+	isValidator bool,
+	addr cosmostypes.AccAddress,
+	ethKeyFromAddress ethcmn.Address,
+) error {
+	if isValidator {
+		log.Debugln("provided ETH address is registered with an orchestrator", addr.String())
+	} else {
+		err := heliosNetwork.SendSetOrchestratorAddresses(ctx, uint64(hyperionID), ethKeyFromAddress.String())
+		if err != nil {
+			return err
+		}
+		addr, isValidator = HasRegisteredOrchestrator(heliosNetwork, uint64(hyperionID), ethKeyFromAddress)
+		if isValidator {
+			log.Debugln("provided ETH address is registered with an orchestrator", addr.String())
+		}
+	}
+	return nil
+}
