424b9a89415f9f58b49c4246702494dec4734f07
jguyet <jguyet.weoohh@gmail.com>
2025-11-03 21:38:51 +0100

    up

--- orchestrator/verifier.go
+++ orchestrator/verifier.go
@@ -7,7 +7,7 @@ import (
 
 func VerifyTxError(ctx context.Context, err string, orchestrator *Orchestrator) (bool, error) {
 	if strings.Contains(err, "account sequence mismatch") {
-		orchestrator.HyperionState.ErrorStatus = "Error: Check Your Node - Maybe Jail or unsync"
+		orchestrator.HyperionState.ErrorStatus = "Check Your Node - Maybe Jail or unsync"
 		orchestrator.ResetHeliosClient()
 	}
 	return true, nil
